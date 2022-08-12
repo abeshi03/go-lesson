7b5c8cc5f653d40da264bc1a1dccb060baad5543
abeshi03 <k_913411@yahoo.co.jp>
2022-08-12 23:06:16 +0900

    slice

--- array.go
+++ array.go
@@ -10,4 +10,12 @@ func main() {
 
 	var b [2]int = [2]int{100, 200}
 	fmt.Println(b)
+
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)
+	fmt.Println(n[2])
+	fmt.Println(n[2:4])
+	fmt.Println(n[:2])
+	fmt.Println(n[2:])
+	fmt.Println(n[1:])
 }
