120ab1dfdbf5dacab841c4d36fcbc6ab2702b687
abeshi03 <k_913411@yahoo.co.jp>
2022-08-16 23:27:49 +0900

    defer

---
+++ defer.go
@@ -0,0 +1,23 @@
+package main
+
+//func foo() {
+//	defer fmt.Println("foo world")
+//
+//	fmt.Println("foo hello")
+//}
+
+func main() {
+	//foo()
+	// 関数の最後に実行される deferは後ろから順番に実行されていく
+	//defer fmt.Println("world")
+	//fmt.Println("hello")
+	//defer fmt.Println(1)
+	//defer fmt.Println(2)
+	//defer fmt.Println(3)
+	//fmt.Println("success")
+
+	//file, _ := os.Open("./lesson.go")
+	//data := make([]byte, 100)
+	//file.Read(data)
+	//fmt.Println(string(data))
+}
