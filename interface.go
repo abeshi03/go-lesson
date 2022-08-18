830940f5e6c78afe15c258c48569a9bc8688420c
abeshi03 <k_913411@yahoo.co.jp>
2022-08-18 09:11:45 +0900

    interface

---
+++ interface.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Human interface {
+	Say() string
+}
+
+type Person struct {
+	Name string
+}
+
+func (p *Person) Say() string {
+	p.Name = "Mr." + p.Name
+	return p.Name
+}
+
+func DriveCar(human Human) {
+	if human.Say() == "Mr.Mike" {
+		fmt.Println("run")
+	} else {
+		fmt.Println("むずい。。。")
+	}
+}
+
+func main() {
+	var mike Human = &Person{"Mike"}
+	//mike.Say()
+	DriveCar(mike)
+}
